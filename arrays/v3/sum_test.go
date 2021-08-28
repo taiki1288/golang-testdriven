e9161f0fbc9538f9a247f965cf3297399217677a
taiki yamashita <taikity0808@gmail.com>
2021-08-28 16:03:58 +0900

    test: update

--- arrays/v3/sum_test.go
+++ arrays/v3/sum_test.go
@@ -26,3 +26,23 @@ func TestSumAll(t *testing.T) {
 		t.Errorf("got %v want %v", got , want)
 	}
 }
+
+func TestSumAllTails(t *testing.T) {
+	t.Run("make the of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{1}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+}
